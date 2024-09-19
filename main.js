61c18f3f0c2a6d1b91018fb0d7fbeb5610355c98
CodeWithIsmail <hmismail360@gmail.com>
2024-09-19 20:47:44 +0600

    done so far

--- main.js
+++ main.js
@@ -1,7 +1,75 @@
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('fa-x');
+//     navbar.classList.toggle('active');
+// }
+
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach.apply(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+//             });
+//         };
+//     });
+// }
+
+// let header = document.querySelector('header');
+// header.classList.toggle('sticky', window.scrollY > 100);
+
+// menuIcon.classList.remove('fa-x');
+// navbar.classList.remove('active');
+
+
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-x');
-    navbar.classList.toggle
+    navbar.classList.toggle('active');
 }
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Software Engineer', 'Competitive Programmer', 'Flutter App Developer', 'Web Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+
+});
+
